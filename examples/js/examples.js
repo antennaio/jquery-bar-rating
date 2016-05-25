7d2c429a41ee8e474c2f5ef024246e58c408dec9
kazik <antenna@antenna.io>
2016-05-25 16:32:12 +0200

    Fractional star ratings - adding an example

--- examples/js/examples.js
+++ examples/js/examples.js
@@ -21,7 +21,7 @@ $(function() {
             initialRating: 'A',
             showValues: true,
             showSelectedRating: false,
-            onSelect:function(value, text) {
+            onSelect: function(value, text) {
                 alert('Selected rating: ' + value);
             }
         });
@@ -51,7 +51,44 @@ $(function() {
         $('#example-bootstrap').barrating({
             theme: 'bootstrap-stars',
             showSelectedRating: false
-        });                
+        });
+
+        var currentRating = $('#example-fontawesome-o').data('current-rating');
+
+        $('.stars-example-fontawesome-o .current-rating')
+            .find('span')
+            .html(currentRating);
+
+        $('.stars-example-fontawesome-o .clear-rating').on('click', function(event) {
+            event.preventDefault();
+
+            $('#example-fontawesome-o')
+                .barrating('clear');
+        });
+
+        $('#example-fontawesome-o').barrating({
+            theme: 'fontawesome-stars-o',
+            showSelectedRating: false,
+            initialRating: currentRating,
+            deselectable: false,
+            onSelect: function(value, text) {
+                $('.stars-example-fontawesome-o .current-rating')
+                    .addClass('hidden');
+
+                $('.stars-example-fontawesome-o .your-rating')
+                    .removeClass('hidden')
+                    .find('span')
+                    .html(value);
+            },
+            onClear: function(value, text) {
+                $('.stars-example-fontawesome-o')
+                    .find('.current-rating')
+                    .removeClass('hidden')
+                    .end()
+                    .find('.your-rating')
+                    .addClass('hidden');
+            }
+        });
     }
 
     function ratingDisable() {
