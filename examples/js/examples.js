4246024d3fd111d537c98e3d62b70ef3b098e8ba
kazik <antenna@antenna.io>
2016-06-02 15:54:05 +0200

    Example improved - allow to deselect a rating

--- examples/js/examples.js
+++ examples/js/examples.js
@@ -70,15 +70,19 @@ $(function() {
             theme: 'fontawesome-stars-o',
             showSelectedRating: false,
             initialRating: currentRating,
-            deselectable: false,
             onSelect: function(value, text) {
-                $('.stars-example-fontawesome-o .current-rating')
-                    .addClass('hidden');
+                if (!value) {
+                    $('#example-fontawesome-o')
+                        .barrating('clear');
+                } else {
+                    $('.stars-example-fontawesome-o .current-rating')
+                        .addClass('hidden');
 
-                $('.stars-example-fontawesome-o .your-rating')
-                    .removeClass('hidden')
-                    .find('span')
-                    .html(value);
+                    $('.stars-example-fontawesome-o .your-rating')
+                        .removeClass('hidden')
+                        .find('span')
+                        .html(value);
+                }
             },
             onClear: function(value, text) {
                 $('.stars-example-fontawesome-o')
